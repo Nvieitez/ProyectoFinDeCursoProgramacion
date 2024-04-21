7ab66ea0573c475f92d921234a583833ffaade92
SantosMSusana <145323629+SusanaSantosM@users.noreply.github.com>
2024-04-22 01:24:02 +0200

    modificación en el ClaseMenuPrincipal.java agregamos un panel

--- 0- PROYECTO FIN DE CURSO/src/ClaseMenuPrincipal.java
+++ 0- PROYECTO FIN DE CURSO/src/ClaseMenuPrincipal.java
@@ -21,40 +21,41 @@
 
         // Entorno Gráfico:
 
-            // Botones:
-
-                // Este botón se encargará de mostrar las opciones de búsqueda para mostrar la lista de músicos/grupos.
-                private JButton BotónVerLista;
-
-                // Este botón se encargará de mostrar las opciones para añadir datos a las diferentes categorías que incluye el programa.
-                private JButton BotónAñadirDatos;
-
-                // Este botón se encargará de mostrar las opciones para eliminar datos de las diferentes categorías que incluye el programa.
-                private JButton BotónEliminarDatos;
-
     // COMIENZO DEL CÓDIGO ====================================================================================================================================
 
         // CONSTRUCTOR MENU PRINCIPAL =========================================================================================================================
 
             // Primero vamos a crear un constructor que recogerá el entorno gráfico de nuestro programa.
             public ClaseMenuPrincipal(){
+            // VISTA DEL ENTORNO GRÁFICO ======================================================================================================================
+
+                // Aquí vamos a declarar las opciones de visionado de la ventana del programa.
 
                 // Dentro del constructor vamos a usar "Super" para poner un título a la ventana que generará nuestro programa al iniciarse.
                 super ("Título Proyecto");
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setLocationRelativeTo(null);
+                setResizable(false);
 
-                // También vamos a crear un layout para que se muestre todo correctamente y nos permita editar el entorno gráfico.
-                setLayout(new FlowLayout());
+                //Creamos un panel para añadir los botones
+                JPanel panel = new JPanel();
+                panel.setLayout(null);
+                panel.setBackground(new Color(163,67,67));
+                add(panel);
 
                 // Ahora vamos a implementar los botones del menú y la funcionalidad de cada uno.
 
             // BOTÓN VER LISTA ================================================================================================================================
 
                 // Primero vamos a introducir el botón al entorno gráfico.
-                JButton BotónVerLista = new JButton("Ver la lista de músicos");
+                JButton botonVerLista = new JButton("Ver la lista de músicos");
+                botonVerLista.setBounds(70,50,200,35);
+                botonVerLista.setBorder(BorderFactory.createLineBorder(Color.BLACK,3));
+                botonVerLista.setForeground(Color.WHITE);
+                botonVerLista.setBackground(new Color(49,54,63));
 
                 // Una vez introducido el botón, vamos a añadir un "Action Listener" para que responda al usuario.
-                BotónVerLista.addActionListener(new ActionListener() {
-                    
+                botonVerLista.addActionListener(new ActionListener() {                    
                     // Dentro del "Action Listener" vamos a declarar un "Action Performed" que le dirá al programa que acción debe de hacer al responder.
                     public void actionPerformed(ActionEvent EventoVerLista){
 
@@ -65,18 +66,22 @@
                 });// Fin del ActionListener.
 
                 // Una vez finalizado el ActionListener vamos a incluir el botón en el layout para que se muestre en el entorno gráfico.
-                add(BotónVerLista);
+                panel.add(botonVerLista);
 
             // BOTÓN AÑADIR DATOS =============================================================================================================================
 
                 // Primero vamos a introducir el botón al entorno gráfico.
-                JButton BotónAñadirDatos = new JButton("Añadir datos");
-
+                //Boton para añadir datos a la biblioteca
+                JButton botonAnadirDatos = new JButton("Añadir datos");
+                botonAnadirDatos.setBounds(70,100,200,35);
+                botonAnadirDatos.setBorder(BorderFactory.createLineBorder(Color.BLACK,3));
+                botonAnadirDatos.setForeground(Color.WHITE);
+                botonAnadirDatos.setBackground(new Color(49,54,63));
+                
                 // Una vez introducido el botón, vamos a añadir un "Action Listener" para que responda al usuario.
-                BotónAñadirDatos.addActionListener(new ActionListener() {
-                    
+                botonAnadirDatos.addActionListener(new ActionListener() {                    
                     // Dentro del "Action Listener" vamos a declarar un "Action Performed" que le dirá al programa que acción debe de hacer al responder.
-                    public void actionPerformed(ActionEvent EventoAñadirDatos){
+                    public void actionPerformed(ActionEvent EventoAnadirDatos){
 
                         // Aquí vamos a crear la respuesta que dará el botón.
                         // TODO
@@ -85,16 +90,20 @@
                 });// Fin del ActionListener.
 
                 // Una vez finalizado el ActionListener vamos a incluir el botón en el layout para que se muestre en el entorno gráfico.
-                add(BotónAñadirDatos);
+                panel.add(botonAnadirDatos)
 
             // BOTÓN ELIMINAR DATOS ===========================================================================================================================
 
                 // Primero vamos a introducir el botón al entorno gráfico.
-                JButton BotónEliminarDatos = new JButton("Eliminar Datos");
+                //Botón para eliminar datos de la biblioteca
+                JButton botonEliminarDatos = new JButton("Eliminar Datos");
+                botonEliminarDatos.setBounds(70,150,200,35);
+                botonEliminarDatos.setBorder(BorderFactory.createLineBorder(Color.BLACK,3));
+                botonEliminarDatos.setForeground(Color.WHITE);
+                botonEliminarDatos.setBackground(new Color(49,54,63));
 
                 // Una vez introducido el botón, vamos a añadir un "Action Listener" para que responda al usuario.
-                BotónEliminarDatos.addActionListener(new ActionListener() {
-                    
+                botonEliminarDatos.addActionListener(new ActionListener() {                    
                     // Dentro del "Action Listener" vamos a declarar un "Action Performed" que le dirá al programa que acción debe de hacer al responder.
                     public void actionPerformed(ActionEvent EventoEliminarDatos){
 
@@ -105,20 +114,8 @@
                 });// Fin del ActionListener.
 
                 // Una vez finalizado el ActionListener vamos a incluir el botón en el layout para que se muestre en el entorno gráfico.
-                add(BotónEliminarDatos);
+                panel.add(botonEliminarDatos);
 
-            // VISTA DEL ENTORNO GRÁFICO ======================================================================================================================
-
-                // Aquí vamos a declarar las opciones de visionado de la ventana del programa.
-
-                // Primero vamos a declarar que la ventana sea visible.
-                setVisible(true);
-
-                // Aquí declaramos el tamaño inicial de la ventana.
-                setBounds(10,20,200,150);
-
-                // Ahora hacemos que el programa termine cuando se cierre la ventana.
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         } // Fin del constructor.
     } // Fin de la clase.
