d690c780f4cfe160a5e45e18acd4e9e7df34c056
ssusibel <dam1@aulaAulainformatica1>
2024-04-26 13:25:17 +0200

    cambiamos algunas cosas

---
+++ 0- PROYECTO FIN DE CURSO/src/Funcionalidad/Albumes.java
@@ -0,0 +1,27 @@
+package Funcionalidad;
+
+public class Albumes {
+    private String nombreAlb;
+    private String fechaLanzamiento;
+
+    public Albumes(){
+        this.nombreAlb = nombreAlb;
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public String getNombreAlb() {
+        return nombreAlb;
+    }
+
+    public void setNombreAlb(String nombreAlb) {
+        this.nombreAlb = nombreAlb;
+    }
+
+    public String getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(String fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+}
