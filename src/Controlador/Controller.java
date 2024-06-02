8fdd671de5fc6c26682e05dd1c303af7c734f266
SantosMSusana <145323629+SusanaSantosM@users.noreply.github.com>
2024-06-02 10:35:49 +0200

    Delete src directory

--- src/Controlador/Controller.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Controller.java to edit this template
- */
-package Controlador;
-
-public class Controller {
-
-}
